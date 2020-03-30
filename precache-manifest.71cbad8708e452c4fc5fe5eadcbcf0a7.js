0aaff0f78fbd31d5cda523603ecea10fa4798d9f
Kyzyl Monteiro <kyzylmonteiro@gmail.com>
2020-03-30 19:20:11 +0530

    Updates

---
+++ precache-manifest.71cbad8708e452c4fc5fe5eadcbcf0a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89aec1d43a7da56251961aea1eda2c12",
+    "url": "/theUrbanCompiler/index.html"
+  },
+  {
+    "revision": "0bc5fb478c59cf48823e",
+    "url": "/theUrbanCompiler/static/css/main.3f63c648.chunk.css"
+  },
+  {
+    "revision": "96abce24e398171a5951",
+    "url": "/theUrbanCompiler/static/js/2.5ad0e896.chunk.js"
+  },
+  {
+    "revision": "8c3b955e6f9c78770e2a37688c24dfc8",
+    "url": "/theUrbanCompiler/static/js/2.5ad0e896.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bc5fb478c59cf48823e",
+    "url": "/theUrbanCompiler/static/js/main.34126521.chunk.js"
+  },
+  {
+    "revision": "700b74c7877cdc7d097e",
+    "url": "/theUrbanCompiler/static/js/runtime-main.99b2cd4d.js"
+  }
+]);
